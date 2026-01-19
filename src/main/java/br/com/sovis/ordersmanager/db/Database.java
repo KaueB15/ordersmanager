ab73cb0d268ea9254c4d777d5046800d120d1475
KaueB15 <kauecardozo3@gmail.com>
2026-01-19 11:47:48 -0300

    feat: create tables scripts

--- src/main/java/br/com/sovis/ordersmanager/db/Database.java
+++ src/main/java/br/com/sovis/ordersmanager/db/Database.java
@@ -2,6 +2,7 @@ package br.com.sovis.ordersmanager.db;
 
 import totalcross.db.sqlite.SQLiteConnection;
 import totalcross.sql.Connection;
+import totalcross.sql.Statement;
 
 public class Database {
     private static Connection connection;
@@ -12,7 +13,58 @@ public class Database {
         }
     }
 
+    public static void createTables() throws Exception {
+        Statement st = connection.createStatement();
+
+        st.execute(
+            "CREATE TABLE IF NOT EXISTS users (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "email TEXT NOT NULL UNIQUE," +
+            "password TEXT NOT NULL)"
+        );
+
+        st.execute(
+            "CREATE TABLE IF NOT EXISTS customer (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "nome TEXT NOT NULL," +
+            "email TEXT," +
+            "phone TEXT," +
+            "createdAt TEXT)"
+        );
+
+        st.execute(
+            "CREATE TABLE IF NOT EXISTS product (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "nome TEXT NOT NULL," +
+            "description TEXT," +
+            "price REAL NOT NULL)"
+        );
+
+        st.execute(
+            "CREATE TABLE IF NOT EXISTS order (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "id_customer INTEGER NOT NULL," +
+            "total_price REAL," +
+            "order_date TEXT," +
+            "status TEXT," +
+            "FOREIGN KEY (id_customer) REFERENCES customer(id))"
+        );
+
+        st.execute(
+            "CREATE TABLE IF NOT EXISTS product_order (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "id_order INTEGER NOT NULL," +
+            "id_product INTEGER NOT NULL," +
+            "price REAL," +
+            "order_date TEXT," +
+            "status TEXT," +
+            "FOREIGN KEY (id_order) REFERENCES order(id)," +
+            "FOREIGN KEY (id_product) REFERENCES product(id))"
+        );
+    }
+
     public static Connection getConnection() {
         return connection;
     }
+
 }
