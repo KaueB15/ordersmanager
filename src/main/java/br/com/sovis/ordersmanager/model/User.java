67b239cada8e5315aae2286cc4ba072fb628bd50
KaueB15 <kauecardozo3@gmail.com>
2026-01-20 09:59:13 -0300

    feat: user model

---
+++ src/main/java/br/com/sovis/ordersmanager/model/User.java
@@ -0,0 +1,40 @@
+package br.com.sovis.ordersmanager.model;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
