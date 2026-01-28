a0dfb95fd640222f41ba9f1590dbc61197b4d86f
KaueB15 <kauecardozo3@gmail.com>
2026-01-28 00:35:12 -0300

    style: login view style modify

--- src/main/java/br/com/sovis/ordersmanager/view/LoginView.java
+++ src/main/java/br/com/sovis/ordersmanager/view/LoginView.java
@@ -7,34 +7,83 @@ import totalcross.ui.Edit;
 import totalcross.ui.Label;
 import totalcross.ui.MainWindow;
 import totalcross.ui.Toast;
+import totalcross.ui.font.Font;
 import totalcross.ui.gfx.Color;
 
 public class LoginView extends Container {
 
     private Label mainLabel = new Label("Login");
+    private Label emailLabel = new Label("E-mail");
+    private Label passwordLabel = new Label("Senha");
+
+    private Container emailBox = new Container();
+    private Container passwordBox = new Container();
+
     private Edit userEmail = new Edit();
     private Edit userPassword = new Edit();
+
     private Button loginButton = new Button("Logar");
     private UserController userController = new UserController();
+
+    int boxWidth = 280;
+    int boxHeight = 50;
+    int padding = 8;
 
     @Override
     public void initUI() {
 
+        setBackColor(Color.getRGB(240, 248, 243));
+
+        mainLabel.setForeColor(Color.getRGB(39, 174, 96));
+        mainLabel.setFont(Font.getFont(true, 22));
         add(mainLabel, CENTER, TOP + 30);
 
-        userEmail.caption = "E-mail";
-        add(userEmail, CENTER, AFTER + 100);
+        emailLabel.setForeColor(Color.getRGB(44, 62, 80));
+        add(emailLabel, LEFT + 40, AFTER + 60);
 
-        userPassword.caption = "Senha";
+        emailBox.setBackColor(Color.WHITE);
+        emailBox.setBorderStyle(Container.BORDER_ROUNDED);
+        emailBox.borderColor = Color.getRGB(39, 174, 96);
+        add(emailBox, CENTER, AFTER + 6, boxWidth, boxHeight);
+
+        Container emailInner = new Container();
+        emailInner.setBackColor(Color.WHITE);
+        emailBox.add(emailInner, LEFT + padding, TOP + padding,
+                boxWidth - padding * 2, boxHeight - padding * 2);
+
+        userEmail.setBackColor(Color.WHITE);
+        userEmail.setForeColor(Color.getRGB(44, 62, 80));
+        userEmail.transparentBackground = false;
+        emailInner.add(userEmail, LEFT, TOP,
+                FILL, boxHeight - padding * 2);
+
+        passwordLabel.setForeColor(Color.getRGB(44, 62, 80));
+        add(passwordLabel, LEFT + 40, AFTER + 16);
+
+        passwordBox.setBackColor(Color.WHITE);
+        passwordBox.setBorderStyle(Container.BORDER_ROUNDED);
+        passwordBox.borderColor = Color.getRGB(39, 174, 96);
+        add(passwordBox, CENTER, AFTER + 6, boxWidth, boxHeight);
+
+        Container passwordInner = new Container();
+        passwordInner.setBackColor(Color.WHITE);
+        passwordBox.add(passwordInner, LEFT + padding, TOP + padding,
+                boxWidth - padding * 2, boxHeight - padding * 2);
+
         userPassword.setMode(Edit.PASSWORD);
-        add(userPassword, CENTER, AFTER + 40);
+        userPassword.setBackColor(Color.WHITE);
+        userPassword.setForeColor(Color.getRGB(44, 62, 80));
+        userPassword.transparentBackground = false;
+        passwordInner.add(userPassword, LEFT, TOP,
+                FILL, boxHeight - padding * 2);
 
-        loginButton.setBackColor(Color.MAGENTA);
+        loginButton.setBackColor(Color.getRGB(39, 174, 96));
         loginButton.setForeColor(Color.WHITE);
-        add(loginButton, CENTER, AFTER + 80);
+        loginButton.setFont(Font.getFont(true, 16));
+        add(loginButton, CENTER, AFTER + 40, 200, 48);
 
         loginButton.addPressListener(event -> {
-            if(userController.login(userEmail.getText(), userPassword.getText())) {
+            if (userController.login(userEmail.getText(), userPassword.getText())) {
                 Toast.show("Login Realizado", 2000);
                 MainWindow.getMainWindow().swap(new HomeView());
             } else {
@@ -42,5 +91,4 @@ public class LoginView extends Container {
             }
         });
     }
-
 }
