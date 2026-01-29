56b8fddfedc6fdf0bff26a226ede2bda8d581221
KaueB15 <kauecardozo3@gmail.com>
2026-01-29 08:47:47 -0300

    feat: field validation customer

--- src/main/java/br/com/sovis/ordersmanager/view/CustomerView.java
+++ src/main/java/br/com/sovis/ordersmanager/view/CustomerView.java
@@ -7,86 +7,126 @@ import totalcross.ui.Edit;
 import totalcross.ui.Label;
 import totalcross.ui.MainWindow;
 import totalcross.ui.Toast;
-import totalcross.ui.event.ControlEvent;
-import totalcross.ui.event.PressListener;
+import totalcross.ui.font.Font;
 import totalcross.ui.gfx.Color;
 
 public class CustomerView extends Container {
 
     private Label mainLabel = new Label("Cadastrar Cliente");
+
     private Edit nameField = new Edit();
     private Edit emailField = new Edit();
     private Edit phoneField = new Edit();
-    private Button registerButton = new Button("Cadastrar Cliente");
+
+    private Button registerButton = new Button("Salvar");
     private Button backButton = new Button("Voltar");
+
+    private Container footer = new Container();
     private CustomerController customerController = new CustomerController();
+
+    int boxWidth = 300;
+    int boxHeight = 50;
+    int padding = 8;
 
     @Override
     public void initUI() {
 
-        add(mainLabel, CENTER, TOP + 10);
+        setBackColor(Color.getRGB(242, 247, 244));
 
-        nameField.caption = "Nome";
-        add(nameField, CENTER, AFTER + 40);
-        
-        emailField.caption = "E-mail";
-        add(emailField, CENTER, AFTER + 40);
+        mainLabel.setForeColor(Color.getRGB(46, 204, 113));
+        mainLabel.setFont(Font.getFont(true, 22));
+        add(mainLabel, CENTER, TOP + 25);
 
-        phoneField.caption = "Telefone";
+        addField("Nome", nameField, AFTER + 40);
+        addField("E-mail", emailField, AFTER + 20);
+        addField("Telefone", phoneField, AFTER + 20);
+
         phoneField.setValidChars("0123456789");
-        add(phoneField, CENTER, AFTER + 40);
 
-        registerButton.setBackColor(Color.MAGENTA);
+        footer.setBackColor(Color.WHITE);
+        add(footer, LEFT, BOTTOM - 70, FILL, 70);
+
+        registerButton.setBackColor(Color.getRGB(46, 204, 113));
         registerButton.setForeColor(Color.WHITE);
-        add(registerButton, CENTER, AFTER + 60);
-        
-        backButton.setBackColor(Color.RED);
+        footer.add(registerButton, LEFT + 10, CENTER, (footer.getWidth() / 2) - 15, 45);
+
+        backButton.setBackColor(Color.getRGB(244, 67, 54));
         backButton.setForeColor(Color.WHITE);
-        add(backButton, CENTER, AFTER + 10);
+        footer.add(backButton, AFTER + 10, SAME, (footer.getWidth() / 2) - 15, 45);
 
-        registerButton.addPressListener(new PressListener() {
-
-            public void controlPressed(ControlEvent e) {
-
-                if (!validateCustomerFields(nameField)) {
-                    return;
-                }
-
-                try {
-                    customerController.createCustomer(nameField.getText(), emailField.getText(), phoneField.getText());
-                    Toast.show("Cliente cadastrado com sucesso!", 2000);
-                    nameField.clear();
-                    emailField.clear();
-                    phoneField.clear();
-                } catch (Exception ex) {
-                    Toast.show("Falha ao cadastrar cliente!", 2000);
-                }
-
-            }
-            
-        });
-
-        backButton.addPressListener(new PressListener() {
-            
-            public void controlPressed(ControlEvent e) {
-                
-                MainWindow.getMainWindow().swap(new ListCustomersView());
-
-            }
-
-        });
-
+        registerButton.addPressListener(e -> saveCustomer());
+        backButton.addPressListener(e ->
+            MainWindow.getMainWindow().swap(new ListCustomersView())
+        );
     }
 
-    private boolean validateCustomerFields(Edit nameEdit) {
+    private void addField(String labelText, Edit field, int posY) {
 
-        if (nameEdit.getText().trim().length() == 0) {
+        Label label = new Label(labelText);
+        label.setForeColor(Color.getRGB(44, 62, 80));
+        add(label, LEFT + 40, posY);
+
+        Container box = new Container();
+        box.setBackColor(Color.WHITE);
+        box.setBorderStyle(Container.BORDER_ROUNDED);
+        box.borderColor = Color.getRGB(46, 204, 113);
+        add(box, CENTER, AFTER + 6, boxWidth, boxHeight);
+
+        Container inner = new Container();
+        inner.setBackColor(Color.WHITE);
+        box.add(inner, LEFT + padding, TOP + padding,
+                boxWidth - padding * 2, boxHeight - padding * 2);
+
+        field.setBackColor(Color.WHITE);
+        field.setForeColor(Color.getRGB(44, 62, 80));
+        field.transparentBackground = false;
+        inner.add(field, LEFT, TOP, FILL, FILL);
+    }
+
+    private void saveCustomer() {
+
+        if (!validateFields()) {
+            return;
+        }
+
+        try {
+            customerController.createCustomer(
+                nameField.getText(),
+                emailField.getText(),
+                phoneField.getText()
+            );
+
+            Toast.show("Cliente cadastrado com sucesso", 2000);
+            nameField.clear();
+            emailField.clear();
+            phoneField.clear();
+
+        } catch (Exception e) {
+            Toast.show("Erro ao cadastrar cliente", 2000);
+        }
+    }
+
+    private boolean validateFields() {
+
+        if (nameField.getText().trim().length() == 0) {
             Toast.show("Nome é obrigatório", 2000);
-            nameEdit.requestFocus();
+            nameField.requestFocus();
+            return false;
+        }
+
+        if (!emailField.getText().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+            Toast.show("E-mail inválido", 2000);
+            emailField.requestFocus();
+            return false;
+        }
+
+        if (phoneField.getText().length() != 11) {
+            Toast.show("Telefone deve ter 11 dígitos", 2000);
+            phoneField.requestFocus();
             return false;
         }
 
         return true;
     }
-
+    
 }
