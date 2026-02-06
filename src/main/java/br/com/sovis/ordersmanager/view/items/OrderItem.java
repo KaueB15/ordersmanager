07d529704a25deb45c1d9b7274226ad705feb828
KaueB15 <kauecardozo3@gmail.com>
2026-02-06 11:19:18 -0300

    feat: new order item

--- src/main/java/br/com/sovis/ordersmanager/view/items/OrderItem.java
+++ src/main/java/br/com/sovis/ordersmanager/view/items/OrderItem.java
@@ -3,13 +3,23 @@ package br.com.sovis.ordersmanager.view.items;
 import br.com.sovis.ordersmanager.dto.OrderLoadingDTO;
 import totalcross.ui.Container;
 import totalcross.ui.Label;
+import totalcross.ui.ScrollContainer;
 import totalcross.ui.gfx.Color;
 
-public class OrderItem extends Container {
+public class OrderItem extends ScrollContainer {
+
+    private Label orderNumber;
+    private Label orderStatus;
+    private Label customerName;
+    private Label orderPrice;
+    private Container row = new Container();
+
     private OrderLoadingDTO order;
 
     public OrderItem(OrderLoadingDTO order) {
+        super(false);
         this.order = order;
+        
     }
 
     @Override
@@ -17,16 +27,36 @@ public class OrderItem extends Container {
 
         setBackColor(Color.WHITE);
 
-        Label label = new Label(
-            "Pedido #" + order.getId() +
-            " | " + order.getStatus() + " - " + 
-            order.getCustomerName() +
-            " | Total: " + order.getTotalValue()
-        );
+        orderNumber = new Label("Pedido #" + order.getId());
+        orderNumber.setForeColor(Color.BLACK);
+        add(orderNumber, LEFT + 10, TOP, FILL - 20, PREFERRED);
 
-        label.setForeColor(Color.BLACK);
+        customerName = new Label(order.getCustomerName());
+        customerName.setForeColor(Color.BLACK);
+        add(customerName, LEFT + 10, AFTER + 5, FILL - 20, PREFERRED);
 
-        add(label, LEFT + 10, CENTER);
+        add(row, LEFT + 10, AFTER + 10, FILL, PREFERRED);
+
+        orderPrice = new Label("R$ " + order.getTotalValue());
+        orderPrice.setForeColor(Color.BLACK);
+        orderStatus = new Label(order.getStatus());
+        orderStatus.setForeColor(order.getStatus().equals("FECHADO") ? Color.RED : Color.GREEN);
+
+        row.add(orderPrice, LEFT, CENTER);
+        row.add(orderStatus, RIGHT - 20, CENTER);
+
+        
+
+        // Label label = new Label(
+        //     "Pedido #" + order.getId() +
+        //     " | " + order.getStatus() + " - " + 
+        //     order.getCustomerName() +
+        //     " | Total: " + order.getTotalValue()
+        // );
+
+        // label.setForeColor(Color.BLACK);
+
+        // add(label, LEFT + 10, CENTER);
     }
 
     public OrderLoadingDTO getOrder() {
